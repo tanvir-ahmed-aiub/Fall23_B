0b8da2edbd7d2de29817fb60a21e9bcd4801ccec
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2023-09-20 16:11:53 +0600

    Intro

---
+++ IntroMVC/IntroMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.CName = "Adv. Programming with .Net";
+            ViewData["Count"] = 40;
+            return View();
+        }
+        public ActionResult Index2() {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            string[] names = new string[] {"Shanto","Saida","Bishal","Mohammad","Nafis" };
+            ViewBag.Names = names;
+
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ IntroMVC/IntroMVC/Controllers/ProfileController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Education() {
+            return View();
+        }
+    }
+}
